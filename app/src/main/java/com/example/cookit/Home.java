c2d57124179671e906d6f1e19e925957c239b771
basit96 <pakkobra007@gmail.com>
2019-11-15 15:58:45 +0500

    last comment from abdul basit

--- app/src/main/java/com/example/cookit/Home.java
+++ app/src/main/java/com/example/cookit/Home.java
@@ -9,11 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Home extends Fragment {
-//Starting First Project
-    //Second coment
-    //comment from abdul basit
-    //comment from bilal
-    //Bilal
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
